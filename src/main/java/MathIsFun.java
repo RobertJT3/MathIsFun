4665f4a5332d14729de066552245fbe2e303d9c4
Gtarnold17 <gtarnold17@gmail.com>
2026-04-07 16:37:39 -0400

    Basics
    Did the easy part so far. Still some errors to work out.

--- src/main/java/MathIsFun.java
+++ src/main/java/MathIsFun.java
@@ -1,11 +1,39 @@
 
 /**
  *
- * @author treyt
+ * @author Tanner Arnold
  */
 public class MathIsFun {
+public class TrackingReports {
+private String studentName;
+private int score;
+private int exercisesCompleted;
+private String teacherComments;
+private String parent;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public TrackingReports(String studentName, int score, int exercisesCompleted){
+String teacherComments (String parent) {
+this.studentName = studentName;
+this.score = score;
+this.exercisesCompleted = exercisesCompleted;
+this.teacherComments = teacherComments;
+this.parent = parent;
+}
+
+public String getStudentName(){
+return studentName;
+}
+public int getScore() {
+return score;
+}
+
+public int getExercisesCompleted() {
+return exercisesCompleted;
+}
+
+public String getTeacherComments() {
+return teacherComments;
+}
+
+}
 }
