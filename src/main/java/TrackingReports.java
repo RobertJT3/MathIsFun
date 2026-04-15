5abd0b53669f5fb1066e5b0cf180cec3f9c8d67b
Gtarnold17 <gtarnold17@gmail.com>
2026-04-15 15:17:45 -0400

    Update TrackingReports.java
    Updated

--- src/main/java/TrackingReports.java
+++ src/main/java/TrackingReports.java
@@ -5,40 +5,70 @@
  * @author Tanner Arnold
  */
 public class TrackingReports {
-private String studentName;
-private int score;
-private int exercisesCompleted;
-private String teacherComments;
-private String parent;
+    private int score;
+    private int exercisesCompleted;
+    private int totalExercises;
+    private String teacherComments;
 
-public TrackingReports(String studentName, int score, int exercisesCompleted, String teacherComments, String parent){
-this.studentName = studentName;
-this.score = score;
-this.exercisesCompleted = exercisesCompleted;
-this.teacherComments = teacherComments;
-this.parent = parent;
-}
+    public TrackingReports(int score, int exercisesCompleted, int totalExercises, String teacherComments) {
+        this.score = score;
+        this.exercisesCompleted = exercisesCompleted;
+        this.totalExercises = totalExercises;
+        this.teacherComments = teacherComments;
+    }
 
-public String getStudentName(){
-return studentName;
-}
-public int getScore() {
-return score;
-}
+    public int getScore() {
+        return score;
+    }
 
-public int getExercisesCompleted() {
-return exercisesCompleted;
-}
+    public int getExercisesCompleted() {
+        return exercisesCompleted;
+    }
 
-public String getTeacherComments() {
-return teacherComments;
-}
-boolean isExercisesCompleted = true;
-if(isExercisesCompleted){
-System.out.println("All exercises are complete for: " + studentName);
-}else{
-System.out.println("The student still has: " + score);
+    public int getTotalExercises() {
+        return totalExercises;
+    }
 
-}
-}    
+    public String getTeacherComments() {
+        return teacherComments;
+    }
 
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setExercisesCompleted(int exercisesCompleted) {
+        this.exercisesCompleted = exercisesCompleted;
+    }
+
+    public void setTotalExercises(int totalExercises) {
+        this.totalExercises = totalExercises;
+    }
+
+    public void setTeacherComments(String teacherComments) {
+        this.teacherComments = teacherComments;
+    }
+
+    public double getProgressPercent() {
+        return (double) exercisesCompleted / totalExercises * 100;
+    }
+
+    public String getFeedback() {
+        if (score >= 90) {
+            return "Excellent work! Keep it up.";
+        } else if (score >= 75) {
+            return "Good job. Keep practicing to improve.";
+        } else {
+            return "Needs more practice and support.";
+        }
+    }
+
+    public void displayReport(String studentName) {
+        System.out.println("Progress Report for: " + studentName);
+        System.out.println("Score: " + score);
+        System.out.println("Exercises Completed: " + exercisesCompleted + "/" + totalExercises);
+        System.out.println("Progress: " + getProgressPercent() + "%");
+        System.out.println("Teacher Comments: " + teacherComments);
+        System.out.println("Feedback: " + getFeedback());
+    }
+}
