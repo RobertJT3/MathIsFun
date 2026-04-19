f6bdbb089a4dbb772d0a56f6addb045ee3f1aa1b
shunter09 <sumirahhunter@gmail.com>
2026-04-19 19:03:03 -0400

    Refactor Teachers class to Teacher with student management
    Refactored Teachers class to Teacher class with additional properties and methods.

--- src/main/java/Teachers.java
+++ src/main/java/Teachers.java
@@ -1,33 +1,94 @@
-
 package com.mycompany.mathisfun;
 
 import java.util.ArrayList;
 
 /**
- *
+ * Creates a teacher with a name, last name, and email address
  * @author Sumirah Hunter
  */
-public class Teachers {
-    private String names;
-    private String lastNames;
+public class Teacher {
+
+    private String username;
+    private String name;
+    private String lastName;
     private String emailAd;
-    
-    public Teachers(){
-        
+    private ArrayList<Student> students = new ArrayList<>();
+
+    /**
+     * default constructor
+     */
+    public Teacher() {
+
     }
-    
-    public Teachers( String names, String lastNames, String emailAd){
-        this.names = names;
-        this.lastNames = lastNames;
+
+    /**
+     * Constructs a teacher with initial values
+     *
+     * @param username username of the teacher
+     * @param name the teachers name
+     * @param lastName the teachers last name
+     * @param emailAd the teachers email address
+     */
+    public Teacher(String username, String name, String lastName, String emailAd) {
+        this.username = username;
+        this.name = name;
+        this.lastName = lastName;
         this.emailAd = emailAd;
-        
-        
+
     }
-    
-   public void viewAssignments(Assignments info) {
-        info.viewAssignments();
+
+    /**
+     * Adds student to students list
+     *
+     * @param s the student that's going to be added
+     */
+    public void addStudent(Student s) {
+        students.add(s);
     }
-    
-    
-    
+
+    /**
+     * gets the list of students
+     *
+     * @return returns the students
+     */
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * gets username
+     *
+     * @return returns username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * gets the name
+     *
+     * @return returns the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * gets last name
+     *
+     * @return returns the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * gets the email
+     *
+     * @return returns the email
+     */
+    public String getEmail() {
+        return emailAd;
+    }
+
 }
