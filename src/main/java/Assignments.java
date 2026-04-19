cde5a86a9fd5d72b5553a06bcbd34ec8b1abea03
shunter09 <sumirahhunter@gmail.com>
2026-04-19 19:00:28 -0400

    Rename Assignments class to Assignment
    Refactored Assignments class to Assignment, updated methods and documentation.

--- src/main/java/Assignments.java
+++ src/main/java/Assignments.java
@@ -3,40 +3,70 @@ package com.mycompany.mathisfun;
 import java.util.ArrayList;
 
 /**
- *
+ * Creates a list of all the assignments
  * @author Sumirah Hunter
  */
-public class Assignments {
-    private final ArrayList<Assignments> allAssignments = new ArrayList<>();
-    
+public class Assignment {
+
+    private final ArrayList<Assignment> allAssignments = new ArrayList<>();
+
     private String title;
     private String description;
-    
-    public Assignments(){
-        
+
+    /**
+     * Default Constructor
+     */
+    public Assignment() {
+
     }
-    
-    public Assignments(String title, String description){
+
+    /**
+     * Constructs an Assignment with values such as title and description
+     *
+     * @param title the title of the Assignment(s)
+     * @param description the description of the Assignment(s)
+     */
+    public Assignment(String title, String description) {
         this.title = title;
         this.description = description;
     }
-    
-    public void addAssignments(String title, String description){
-        Assignments a = new Assignments(title, description);
+
+    /**
+     * Creates a new assignment, adds it to the array, and prints it
+     *
+     * @param title the title of the assignment
+     * @param description the description of the assignment
+     */
+    public void addAssignments(String title, String description) {
+        Assignment a = new Assignment(title, description);
         allAssignments.add(a);
         System.out.print(a);
     }
-    
-    public void viewAssignments(){
-        for(Assignments a : allAssignments){
+
+    /**
+     * Prints every assignment in allAssignments, allows for the assignments to
+     * be viewed
+     */
+    public void viewAssignments() {
+        for (Assignment a : allAssignments) {
             System.out.println(a);
         }
     }
-    
-      public ArrayList<Assignments>getAllAssignments(){
+
+    /**
+     * Gets the list of all the assignments
+     *
+     * @return returns all assignments
+     */
+    public ArrayList<Assignment> getAllAssignments() {
         return allAssignments;
     }
-      
+
+    /**
+     * String representation of the assignment object
+     *
+     * @return returns the title and description formatted
+     */
     @Override
     public String toString() {
         return title + "\n • " + description;
