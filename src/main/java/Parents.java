7aa98e9b66b36f71e55afba954f043c083c1c288
shunter09 <sumirahhunter@gmail.com>
2026-04-19 19:01:31 -0400

    Refactor Parents class to Parent with new properties
    Renamed the class from 'Parents' to 'Parent' and updated its properties and methods.

--- src/main/java/Parents.java
+++ src/main/java/Parents.java
@@ -1,28 +1,74 @@
-
 package com.mycompany.mathisfun;
 
-
-
 /**
+ * Creates a parent with name, last name, and email address
  *
  * @author Sumirah Hunter
  */
-public class Parents {
-    private String names;
-    private String lastNames;
+public class Parent {
+
+    private String username;
+    private String name;
+    private String lastName;
     private String emailAd;
-    
-    
-    public Parents(){
-        
+
+    /**
+     * default constructor
+     */
+    public Parent() {
+
     }
-    
-    public Parents(String names, String lastNames, String emailAd){
-        this.names = names;
-        this.lastNames = lastNames;
+
+    /**
+     * Constructs a parent with initial values
+     *
+     * @param username username of the parent
+     * @param name the parents name
+     * @param lastName the parents last name
+     * @param emailAd the parents email address
+     */
+    public Parent(String username, String name, String lastName, String emailAd) {
+        this.username = username;
+        this.name = name;
+        this.lastName = lastName;
         this.emailAd = emailAd;
+
     }
-    
-    
-    
+
+    /**
+     * gets username
+     *
+     * @return returns username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * gets the name
+     *
+     * @return returns the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * gets last name
+     *
+     * @return returns the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * gets the email
+     *
+     * @return returns the email
+     */
+    public String getEmail() {
+        return emailAd;
+    }
+
 }
